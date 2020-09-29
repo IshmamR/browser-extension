e82bb370c0031663419b82cf908abcfa19ffa82b
IshmamR <ishmam785@gmail.com>
2020-09-29 22:18:01 +0600

    version 2.0.0

--- popup.js
+++ popup.js
@@ -1,135 +1,153 @@
-/*async function sendO () {
-	var gotItem = browser.storage.local.get();
-	gotItem.then(gett, errr);
-	async function gett (item) {
-		if(item["objectToSend"] == undefined){	
-			var objectsent = {"is": ['atia']};
-			var objectToSend = JSON.stringify(objectsent);
-			await browser.storage.local.set({objectToSend});
-		}
-	}
-	function errr (error) {
-		// bla bla		
-	}
-}
-sendO();*/
-var arrayToChoose = {
+// console.log("Version 2.0");
+var data = {
 	'gm': ['https://mail.google.com/', 'fa','fa-envelope','text-red-500','hover:text-red-600'],
 	'fb': ['https://facebook.com/', 'fa','fa-facebook-square','text-blue-500','hover:text-blue-600'],
 	'tt': ['https://twitter.com/', 'fa','fa-twitter-square','text-blue-400','hover:text-blue-500'],
 	'yt': ['https://youtube.com/', 'fa','fa-youtube','text-red-600','hover:text-red-700']
 };
-var itemArray = {};
-var main = document.querySelector('.main'); // upper part
-var links = document.querySelector('.links'); // chosen icons
-var choose = document.querySelectorAll('.choose'); // icons to choose
-
-// onclick icon choose function
-choose.forEach(item => {
-	item.addEventListener('click', iPush);
+// display toggle for choose links
+var butt = document.querySelector('.cross');
+butt.addEventListener('click', ()=> {
+	var icons = document.querySelector('.icons');
+	var txt = document.querySelector('.txt');
+	if(icons.style.display == 'flex') {
+		icons.style.display = 'none';
+		butt.innerHTML = '&#8690;';
+		txt.classList.remove('text-lg');
+		txt.classList.add('text-sm');
+	} else {
+		icons.style.display = 'flex';
+		butt.innerHTML = '&times;';
+		txt.classList.remove('text-sm');
+		txt.classList.add('text-lg');
+	}
 });
-var obj = {}; // obj to push to extension local storage
 
-// choose function
-function iPush () {
-	var gotItem = browser.storage.local.get();
-	gotItem.then(got, err);
-	// console.log(this);
-	var ipush_this = this;
-	function got(item) {
-		// console.log(item);
-		var key = ipush_this.id;
-		var gottenObject;
-		if(item["objectToSend"] != undefined){
-			gottenObject = JSON.parse(item["objectToSend"]);
-		}
-		// console.log(gottenObject);
-		if (!(key in gottenObject)) { // checks if key is already in storage or not
-			obj[key] = ipush_this.classList;
-			create(ipush_this);
-			console.log(key);
-			console.log(gottenObject);
-			sendToLocal(gottenObject); // funtion to push to local storage
-		}
-	}
-	function err(error){
-		console.log(error);
-	}
-}
-
-// create link after chosen
-function create (item) {
-	var a = document.createElement('a');
-	var i = item; // appending all info about item to variable i
-	item.remove(); // removes item from choose section
-	i.classList.add("fa-lg");
-	i.removeEventListener('click', iPush); // removing event listener when chosen
-	// console.log(links);
-	a.href = arrayToChoose[i.id][0]; // see line 1
-	a.target = '_blank'; // opens in another tab
-	a.appendChild(i);
-	links.appendChild(a);
-}
-
-// function to send data into the local storage
-async function sendToLocal(object) {
-	console.log(object);
-	var objectToSend = JSON.stringify(object);
-	await browser.storage.local.set({objectToSend});
-	// console.log(objectToSend);
-}
-
-// ___________________________
+// ______________START_____________
 // get data from local storage
-var gettingItem = browser.storage.local.get();
-gettingItem.then(onGot, onError);
+var getDataFromStore = browser.storage.local.get();
+getDataFromStore.then(onGot, onError);
+
 function onGot(item){ // if no error occurs
-	var gotObj = JSON.parse(item["objectToSend"]);
-	// console.log(gotObj);
-	for(var objs in gotObj) {
-		createOnDOM(objs);
+	// console.log(item['links']);
+	if (item['links'] == undefined) {
+		var object = {}; // if links does not exists in storage, creates an empty object in storage
+		sendToLocal(object);
+	} else {
+		console.log(item['links'] + "--These are the links from storage");
+	}
+	var linksData = item['links'];
+	if ( linksData != {} ) {
+		createLinks(linksData); // creates links from storage
+		createIconsToChoose(linksData); // sends datas to check with created icons to choose
 	}
 }
 function onError(error){ // display error if any
 	console.log(error);
 }
-// =============================
+// ===============END==============
 
-// display toggle for choose links 
-var butt = document.querySelector('.cross');
-butt.addEventListener('click', ()=> {
-	var icons = document.querySelector('.icons');
-	var txt = document.querySelector('.txt');
+// _Function to store data in storage
+async function sendToLocal(objectToSend) {
+	var links = JSON.stringify(objectToSend);
+	await browser.storage.local.set({links});
+	console.log(links + "--was sent to storage");
+}
+// _END_____
+
+// Function to create icons to choose
+function createIconsToChoose(icon_data) {
+	var iconsData = JSON.parse(icon_data);
+	var iconsToChoose = document.querySelector('.icons');
 	
-	if(icons.style.display == 'none') {
-		icons.style.display = 'flex';
-		butt.innerHTML = '&times;';
-		txt.classList.remove('text-sm');
-		txt.classList.add('text-lg');
-	} else {
-		icons.style.display = 'none';
-		butt.innerHTML = '&#8690;';
-		txt.classList.remove('text-lg');
-		txt.classList.add('text-sm');
+	for(id in data) {
+		if (Object.keys(iconsData).includes(id)) {
+			console.log(id + " exists in storage");
+		} else {
+			console.log(id + " does not exist in storage");
+			var i = document.createElement('i');
+			var c = data[id];
+			for (var j = 1; j < c.length; j++) {
+				i.classList.add(c[j]);
+			}
+			i.classList.add('choose');
+			i.setAttribute('data-id', id);
+			i.addEventListener('click', linkPush); // function to choose
+			iconsToChoose.append(i);
+		}
 	}
-});
+}
+createIconsToChoose();
+// ___x___
 
-function createOnDOM(data) {
+// When icon is clicked this function takes data and creates link on the links section
+function linkPush() {
+	const linksDiv = document.querySelector('.links');
+	var iLink = this;
+	iLink.classList.remove('choose');
+	iLink.id = iLink.getAttribute('data-id');
+
 	var a = document.createElement('a');
-	var i = document.createElement('i');
-	// console.log(data);
-	// console.log(arrayToChoose[data]);
-	// console.log(arrayToChoose);
-	for(var key = 1; key < arrayToChoose[data].length; key++) {
-		// console.log(arrayToChoose[data][key]);
-		i.classList.add(arrayToChoose[data][key]);
+	a.href = data[iLink.id][0];
+	a.target = '_blank';
+	a.append(iLink);
+	linksDiv.append(a);
+	// console.log(iLink);
+	updateData(iLink); // function to update data in storage
+}
+// ___***___
+
+// Links to display
+function createLinks(linksObject) {
+	var object = JSON.parse(linksObject);
+	var linksHTML = document.querySelector('.links');
+	for(id in object) {
+		var a = document.createElement('a');
+		var i = document.createElement('i');
+		i.id = id;
+		for (var j = 1; j < data[id].length; j++) {
+			i.classList.add(data[id][j]);
+			// console.log(data[id][j]);
+		}
+		a.href = data[id][0];
+		a.target = '_blank';
+		a.append(i);
+		linksHTML.append(a);
 	}
-	a.href = arrayToChoose[data][0]; // see line 1
-	a.target = '_blank'; // opens in another tab
-	a.appendChild(i);
-	links.appendChild(a);
 }
-function cons () {
-	console.log(obj);
+
+// Update data to storage
+function updateData(element) {
+	// console.log(element.classList);
+	
+	var getDataFromStorage = browser.storage.local.get();
+	getDataFromStorage.then(got, onError);
+	
+	var element_data = {};
+	var element_classes = element.classList;
+	var element_id = element.id;
+	function got(item) {
+		var linksData = item['links'];
+		var arr = [];
+		arr[0] = data[element_id][0];
+		element_classes.forEach(el_class => {
+			arr.push(el_class);
+		})
+		var object1 = JSON.parse(linksData);
+		element_data = object1;
+		element_data[element_id] = arr;
+		// console.log(element_data);
+		sendToLocal(element_data);
+	}
 }
-// setInterval(cons, 3000);
+
+
+
+/*
+var btn = document.querySelector('#ibtn');
+btn.addEventListener('click', ()=> {
+	// var dataTest = data;
+	// sendToLocal(dataTest);
+	console.log("it works!");
+});
+*/
